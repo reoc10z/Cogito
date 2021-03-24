3e4d9328b65ae02a4225439502835f51660b9597
reoc10z <rafael.theoc@gmail.com>
2021-03-24 11:06:24 -0500

    new times for time ball and time for pattern. Thus, more balls were added and two more patterns to level medium

--- Assets/Scripts/CogitoController.cs
+++ Assets/Scripts/CogitoController.cs
@@ -68,18 +68,22 @@ public class CogitoController : MonoBehaviour
 
     private short[] _listBallPosition_level1 = new short[]
     {
-        -1, 5, 1, 4, -3, -1, 2, -3, 6, -4, -6, -4, -2, 4, -3, -6, -1,
-        -3, -2, 1, -1, -2, 1, 3, 2, 6, -2, 6, -5, -4, -2, -4, -5, 3,
-        -3, -1, 6, 5, -4, 3, 1, 5, 6, 3, 1, 3, -1, 5, 6, 3, -3,
-        3, -1, 3, -3, -4, -2, 4, -5, -1
+        -5,  1, -6,  6,  3, -5, -1, -3, -4, -1, -2, -5, -4, -2,  6, -5, -2,
+        3,  1, -2, -5, -2,  6,  5,  2, -1,  3,  2, -3,  5, -4, -5,  1, -4,
+        2, -1,  2, -6, -5,  2,  4,  3, -1,  5, -2, -1,  2,  6, -2,  2, -2,
+        -3,  6, -2,  4,  2, -2,  5,  2, -3, -6, -3,  2,  6,  4, -1,  6,  5,
+        4, -3, -5, -2, -1, -5, -4,  1, -2, -6,  4, -1, -6, -3, -4,  3, -4,
+        6,  3,  6,  5, -3
     };
 
     private short[] _listBallPosition_level2 = new short[]
     {
-        -2, -4, -2, 2, -6, -1, -3, 1, -1, -2, 4, 6, 5, 1, -1, 6, -5,
-        -2, -4, 4, 1, 4, -3, 1, -3, 6, -1, -2, -1, 5, 1, 2, -2, 6,
-        -2, -1, 2, 1, -4, -1, 3, -6, 2, 4, -5, 3, -4, 3, -2, 1, -6,
-        4, -5, 4, 3, -5, 4, 6, 5, 6
+        2,  1, -2, -1, -5,  1, -4,  3, -3, -6,  6,  1, -1, -6, -3,  3, -3,
+        -5, -3,  1, -3,  4,  5,  2, -1, -3, -4,  4,  3, -5, -6,  2, -2,  2,
+        -4, -3, -1, -3,  4,  5, -3, -2,  3,  4, -1,  2,  5,  3, -2, -3,  5,
+        -2, -3, -6, -5,  2,  3, -1,  3, -6,  3, -1,  5,  3, -5, -2, -5,  1,
+        5,  4,  3, -4,  5, -1,  4, -6, -3, -5, -3,  4,  6,  2,  3, -3,  6,
+        -4,  5, -5, -1,  2,  3,  1, -6,  6,  4, -3, -2, -5,  5, -1,
     };
 
     private short[] _listBallPosition;
@@ -134,6 +138,8 @@ public class CogitoController : MonoBehaviour
         new bool[25] { true,  true,  true,  true,  false,  true,  true,  true,  true,  false,  false,  true,  true,  false,  true,  true,  false,  true,  true,  true,  true,  false,  true,  true,  true, },
         new bool[25] { true,  true,  true,  false,  true,  true,  false,  false,  true,  true,  true,  true,  true,  false,  false,  true,  true,  true,  true,  true,  true,  true,  true,  false,  true, },
         new bool[25] { true,  true,  false,  true,  true,  true,  false,  true,  true,  true,  true,  true,  false,  false,  true,  true,  true,  true,  true,  true,  true,  false,  true,  false,  true, },
+        new bool[25] { true,  false,  true,  false,  true,  true,  true,  true,  true,  true,  true,  false,  true,  false,  true,  true,  true,  false,  false,  true,  true,  true,  true,  true,  true, },
+        new bool[25] { false,  false,  true,  true,  true,  true,  true,  true,  true,  true,  true,  true,  true,  false,  true,  true,  false,  true,  true,  false,  true,  false,  true,  true,  true, },
         new bool[25], // pattern_end: it is set to false. This pattern HAS TO be used at the end of all pattern lists of each level
     };
 
