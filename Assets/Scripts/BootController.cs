808775c5779543dc86fa7129dafa50b085f556cb
reoc10z <rafael.theoc@gmail.com>
2021-04-23 16:01:46 -0500

    bootcontroller: script when booting the app

---
+++ Assets/Scripts/BootController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class BootController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // steps file
+        WriteFile( CreateFile("SettingsStep.txt") , "1", "r" );
+        GoToMenu();
+    }
+
+    private void GoToMenu()
+    {
+        Loader.Load(Loader.Scene.MenuScene);
+    }
+    
+    // if file already exists, it returns that path.
+    private string CreateFile(string fileName)
+    {
+        string filePath = GetPathFile(fileName);
+        
+        if (!File.Exists(filePath)) {
+            //Create File if it doesn't exist
+            File.WriteAllText(filePath, "");
+        }
+        
+        return filePath;
+    }
+    
+    // type = a, for appending
+    // type = r, for replace
+    private void WriteFile(string filePath, string msg, string type)
+    {
+        if (type == "a")
+            File.AppendAllText(filePath, msg);
+        else if (type=="r")
+            File.WriteAllText(filePath, msg);
+    }
+    
+    private string GetPathFile(string fileName)
+    {
+        string filePath;
+        if ( SystemInfo.deviceModel == "PC")
+        {
+            filePath =  Application.dataPath + "/" + fileName;
+        }
+        else
+        {
+            filePath = Application.persistentDataPath + "/" + fileName;
+        }
+
+        return filePath;
+    }
+    
+}
